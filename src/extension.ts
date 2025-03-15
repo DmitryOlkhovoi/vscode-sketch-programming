027509a30f1cb5b3bce511993ded833b7f0b64b2
unknown <auuine@gmail.com>
2025-03-15 21:03:35 +0200

    Do not send files for transpiling multiple times

--- src/extension.ts
+++ src/extension.ts
@@ -21,7 +21,10 @@ export async function activate(context: vscode.ExtensionContext) {
 	console.log('Sketch Programming -- LLM Transpiler extension is now active!');
 
 	const workspaces: Record<string, Workspace> = {};
+	const transpiling = new Set<string>();
+	const isTranspilingDirty = new Set<string>();
 	let sketchName: string | null = null;
+	let root: string | undefined;
 
 	function updateSketchName(editor: vscode.TextEditor) {
 		if (!editor) {
@@ -38,33 +41,73 @@ export async function activate(context: vscode.ExtensionContext) {
 		}
 	}
 
+	async function updateRoot(editor: vscode.TextEditor) {
+		if (!editor) {
+			return;
+		}
+
+		root = await findSketchProjectRoot(editor.document.uri.fsPath);
+	}
+
 	vscode.window.onDidChangeActiveTextEditor((editor: vscode.TextEditor | undefined) => {
 		if (editor) {
 			updateSketchName(editor)
+			console.log(`Sketch-programming Extension: onDidChangeActiveTextEditor Active sketch - ${sketchName}`)
+
+			if (sketchName) {
+				updateRoot(editor);
+			}
 		}
 	})
 
-	vscode.workspace.onDidSaveTextDocument(async (document: vscode.TextDocument) => {
+	vscode.workspace.onWillSaveTextDocument(async ({ document }) => {
+		console.log(`Sketch-programming Extension: onDidSaveTextDocument - ${document.uri.fsPath}, isDirty: ${document.isDirty}, isTranspiling: ${transpiling.has(document.uri.fsPath)}, isTranspilingDirty: ${isTranspilingDirty.has(document.uri.fsPath)}`)
+		
+		if (transpiling.has(document.uri.fsPath)) {
+			isTranspilingDirty.add(document.uri.fsPath);
+			
+			vscode.window.showErrorMessage(`Sketch-programming Extension: onDidSaveTextDocument - ${document.uri.fsPath} is not transpiled yet. This onSave event is ignored.`);
+			return
+		}
+
+		if (!document.isDirty && !isTranspilingDirty.has(document.uri.fsPath)) {
+			return
+		}
+
+		console.log(`Sketch-programming Extension: Will save document - ${document.uri.fsPath}`)
+		
 		if (vscode.window.activeTextEditor && !sketchName) {
 			updateSketchName(vscode.window.activeTextEditor);
 		}
 
 		if (sketchName) {
-			const root = await findSketchProjectRoot(document.uri.fsPath);
+			if (!root) {
+				await updateRoot(vscode.window.activeTextEditor!);
+			}
 
 			if (root) {
+				console.log(`Sketch-programming Extension: Config found in the root`);
 				let currentWorkspace = workspaces[root];
 				
 				if (!currentWorkspace) {
                     workspaces[root] = currentWorkspace = new Workspace(root);
 					await currentWorkspace.initialize();
                 }
-				
-				const traspiledCode = await currentWorkspace.transpile(document.getText());
 
-				if (traspiledCode) {
-					console.log(`Transpiled code: ${traspiledCode}`);
-					currentWorkspace.saveFile(document, traspiledCode);
+				try {
+					transpiling.add(document.uri.fsPath)
+					isTranspilingDirty.delete(document.uri.fsPath);
+					const traspiledCode = await currentWorkspace.transpile(document.getText());
+
+					if (traspiledCode) {
+						console.log(`Sketch-programming Extension: transpiled code - ${traspiledCode}`);
+						currentWorkspace.saveFile(document, traspiledCode);
+					}
+				} catch (error) {
+					isTranspilingDirty.add(document.uri.fsPath);
+					vscode.window.showErrorMessage(`Sketch-programming Extension: Error transpiling code (${document.uri.fsPath}) - ${error}`);
+				} finally {
+					transpiling.delete(document.uri.fsPath);
 				}
             } else {
 				vscode.window.showErrorMessage(`Sketch-programming Workspace: No config in the root`);
